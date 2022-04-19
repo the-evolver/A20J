58cd3bdf1d09c060083dc52f1d4fb0d0d505cf65
ATUL KUMAR <69716309+the-evolver@users.noreply.github.com>
2022-04-19 19:29:34 +0530

    Update #100_A_Adding_digits.cpp

--- #100_A_Adding_digits.cpp
+++ #100_A_Adding_digits.cpp
@@ -5,12 +5,12 @@ int main(){
 
 	int a , b , n;
 	cin>>a>>b>>n;
-    bool hai_ki_nhi = false;
+    bool prsnt = false;
 	
 	for(int i = 0 ; i < 10 ; i++){
 
 		if((a * 10 +  i) % b == 0){
-			hai_ki_nhi = true;
+			prsnt = true;
 			cout<<a * 10 + i;
 			string s(n-1,'0');
 			
@@ -21,64 +21,10 @@ int main(){
 
 
 	}
-   if(hai_ki_nhi != true){
+   if(prsnt != true){
    	   cout<<-1<<endl;
    }
 
   return 0;
 
 }
-
-
-
-
-
-
-
-
-
-// ->  dont always think old school kbhi bs trick se bn jata hai 
-// badi badi baten aur bada pav khate  - ! -
-//                                      ~~~
-// #include <iostream>
-// using namespace std;
-
-
-// // 100000 10 64479
-
-// int main(){
-
-// 	int a,b,n;
-// 	cin>>a>>b>>n;
-// 	int cpy_a = a;
-
-// 	while(n--){
-// 		 a = a * 10;
-// 		 int rem = a % b ;
-//          if(rem == 0){
-//          	continue;
-//          }
-
-// 		 rem = b - rem ;
-// 		 if(rem > 9){
-// 		 	a = a / 10;
-// 		 	break;
-// 		 }
-// 		 else{
-//            a = a + rem ;
-
-// 		 }
-
-
-// 	}
-// 	//cout<<a<<endl;
-// 	if(cpy_a == a){
-// 		cout<<"-1"<<endl;
-// 	}else{
-// 		cout<<a<<endl;
-// 	}
-
-// 	return 0;
-
-// }
-
